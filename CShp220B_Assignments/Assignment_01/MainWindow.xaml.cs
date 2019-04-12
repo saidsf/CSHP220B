af69af3ae77c6751d6901dcd6c9dcfcebda719c7
Said Fathelbab <sfathelbab@hotmail.com>
2019-04-11 19:20:56 -0700

    Assignment01: Enable Button only when a textbox and a passwordbox both have text

---
+++ CShp220B_Assignments/Assignment_01/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableDisableButton();
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bingo!");
+        }
+
+        private void PasswordBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            EnableDisableButton();
+        }
+
+        private void EnableDisableButton()
+        {
+            if (uxName.Text != string.Empty && uxPassword.ToString() != string.Empty)
+                uxSubmit.IsEnabled = true;
+            else
+                uxSubmit.IsEnabled = false;
+
+        }
+    }
+}

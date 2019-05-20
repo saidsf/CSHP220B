8a4a2104549672e2da70e3d7fdd2ebc0c08d6f6f
Said Fathelbab <sfathelbab@hotmail.com>
2019-05-19 23:13:13 -0700

    Assignment05:

---
+++ CShp220B_Assignments/Assignment_05/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Assignment_05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        T3 t3Game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            t3Game = new T3();
+            uxTurn.Text = GameStatus.GameStatus_X_Trun.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!t3Game.IsGameOver())
+            {
+                Button tile = sender as Button;
+                if (tile != null)
+                {
+                    string rowCol = tile.Tag.ToString();
+
+                    GameStatus result = t3Game.MakeA_Move(rowCol, tile);
+                    uxTurn.Text = result.ToString();
+                    if (t3Game.IsGameOver())
+                        uxGrid.IsEnabled = false;
+                }
+            }
+        }
+
+        private void uxExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new MainWindow();
+            Application.Current.MainWindow = window;
+            Close();
+            window.Show();
+        }
+    }
+}
---
+++ CShp220B_Assignments/Assignment_05/T3.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Assignment_05
+{
+    enum GameStatus { GameStatus_Draw, GameStatus_XWin, GameStatus_OWin, GameStatus_X_Trun, GameStatus_O_Trun };
+    class T3
+    {
+        const char OPEN_TILE = ' ';
+        const char X = 'X';
+        const char O = 'O';
+        int MAX_MOVES;
+        char[,] data;
+        int moveCount = 0;
+        int rowSize = 0;
+        int colSize = 0;
+        bool playerXTurn = true;
+        bool gameOver = false;
+        GameStatus currentStatus = GameStatus.GameStatus_X_Trun;
+
+        public T3(int row = 3, int col = 3)
+        {
+            rowSize = row;
+            colSize = col;
+            MAX_MOVES = row * col;
+            moveCount = 0;
+            playerXTurn = true;
+            gameOver = false;
+
+            data = new char[row, col];
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < col; j++)
+                    data[i, j] = OPEN_TILE;
+        }
+
+        private void SplitIndex(string index, out int row, out int col)
+        {
+            var parts = index.Split(',');
+            if (parts.Length != 2)
+                throw new ArgumentException("The provided index is not valid");
+
+            row = int.Parse(parts[0]);
+            col = int.Parse(parts[1]);
+        }
+
+        public char this[string index]
+        {
+            get
+            {
+                int row, col;
+                SplitIndex(index, out row, out col);
+                return data[row, col];
+            }
+            set
+            {
+                int row, col;
+                SplitIndex(index, out row, out col);
+                if (data[row, col] == OPEN_TILE)
+                {
+                    data[row, col] = value ;
+                }
+            }
+        }
+
+        public char this[int row, int col]
+        {
+            get { return data[row, col]; }
+            set
+            {
+                if (data[row, col] == OPEN_TILE)
+                {
+                    data[row, col] = value;
+                }
+            }
+        }
+
+        bool IsWinner (string index, char playerMark)
+        {
+            int row, col;
+            SplitIndex(index, out row, out col);
+            for (int c = 0; c < colSize; c++)
+            {
+                if (this[row, c] != playerMark)
+                    return false;
+            }
+            return true;
+        }
+
+        public GameStatus MakeA_Move(string index, Button tile)
+        {
+            char playerMark = playerXTurn ? X : O;
+
+            if (moveCount++ < MAX_MOVES)
+            {
+                tile.Content = playerMark;
+                this[index] = playerMark;
+                if (IsWinner(index, playerMark))
+                {
+                    gameOver = true;
+                    return playerXTurn ? GameStatus.GameStatus_XWin : GameStatus.GameStatus_OWin;
+                }
+            }
+
+            if (moveCount >= MAX_MOVES)
+            {
+                gameOver = true;
+                return GameStatus.GameStatus_Draw;
+            }
+
+            playerXTurn = !playerXTurn;
+            return playerXTurn ? GameStatus.GameStatus_X_Trun : GameStatus.GameStatus_O_Trun;
+        }
+
+        public bool IsGameOver()
+        {
+            return gameOver;
+        }
+
+        public GameStatus GetGameStatus()
+        {
+            return currentStatus;
+        }
+    }
+}
